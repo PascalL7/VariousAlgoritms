f48387ffb08b4f02a63a6b6e193a4c575641cdd6
Pascal LEVALLOIS <pascaljob@protonmail.com>
2022-08-23 18:25:15 +0200

    Add Unary Translator method

--- VariousAlgorithms.php
+++ VariousAlgorithms.php
@@ -12,9 +12,35 @@ class VariousAlgorithms
     {
         $elements = ['A', 'B', 'C', 'D', 'E', 'F'];
 
-        return array_filter(array_map(fn ($valueOfElements) => array_slice($elements, array_search($valueOfElements, $elements) * $sliceCount, $sliceCount), $elements));
+        return array_filter(array_map(fn($valueOfElements) => array_slice($elements, array_search($valueOfElements, $elements) * $sliceCount, $sliceCount), $elements));
+    }
+
+    /*
+     * Objective : create an unary translator for a message in alphanumeric
+     */
+    static public function unaryTranslator()
+    {
+        $message = stream_get_line(STDIN, 100 + 1, "\n");
+        $msgLen= strlen($message);
+
+        $bits = '';
+        for ($i = 0; $i < $msgLen; $i++ ) {
+            $bits .= sprintf("%07d", decbin(ord($message[$i])));
+        }
+        error_log("string in binary format: $bits\n\noutput:");
+        $matches = [];
+        $occurences = preg_match_all('/0+|1+/', $bits, $matches);
+        $final = '';
+
+        foreach ($matches as $match)
+        {
+            for ($i = 0; $i < count($match); $i++)
+            {
+                $final .= (substr($match[$i], 0, 1) === "1" ? " 0" : " 00");
+                $final .= ' ' . str_pad('', strlen($match[$i]), '0');
+            }
+        }
+
+        return ltrim($final) . PHP_EOL;
     }
 }
-
-var_dump(VariousAlgorithms::arrayChunk(3));
-
