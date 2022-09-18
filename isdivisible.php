53bf16e847c609947ef1b54931c21b00c95a34ff
Pascal L <PascalJob@protonmail.com>
2022-09-18 11:34:03 +0200

    Update isdivisible.php

--- isdivisible.php
+++ isdivisible.php
@@ -1,3 +1,3 @@
-function is_divisible($n, $x, $y) {
-  return ($n % $x === 0 && $n % $y === 0) ? true : false;
+function is_divisible($n, $x, $y) :bool {
+  return $n % $x === 0 && $n % $y === 0;
 }
